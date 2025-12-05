277f8c4880fffcacc5d770854fd109e8330dc682
Qray <ilyasch2020@gmail.com>
2025-12-05 17:53:43 +0300

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const yearPlaceholder = document.getElementById('year-placeholder');
-
-if (yearPlaceholder) {
-  yearPlaceholder.textContent = new Date().getFullYear();
-}
-
-// TODO: здесь будет логика сайта Galenite
