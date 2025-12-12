9fca4e3bf111f47e66164fa4dc14beb2fb63dd5c
Qray <ilyasch2020@gmail.com>
2025-12-12 19:31:37 +0300

    Update firebase.js

--- shared/firebase.js
+++ shared/firebase.js
@@ -1,15 +1,18 @@
+// /shared/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
-export const app = initializeApp({
-  apiKey: "AIzaSyCP4G9o0g0pqPwoIYBJ3UEZyrPFt1PEDbE",
-  authDomain: "galenite-f1b8e.firebaseapp.com",
-  projectId: "galenite-f1b8e",
-  storageBucket: "galenite-f1b8e.appspot.com",
-  messagingSenderId: "97488613838",
-  appId: "1:97488613838:web:24bbacd61a69e3fe8ffb9f"
-});
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCI1Vp5CSBu0veADOHtuUcFATSaOnXPswc",
+  authDomain: "galenite-glnt.firebaseapp.com",
+  projectId: "galenite-glnt",
+  storageBucket: "galenite-glnt.firebasestorage.app",
+  messagingSenderId: "94081808482",
+  appId: "1:94081808482:web:2f5f4842c293c18621f93b",
+};
 
+export const app = initializeApp(FIREBASE_CONFIG);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
