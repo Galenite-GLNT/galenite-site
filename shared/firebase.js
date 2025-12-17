106fc61fe6514f34d363584c2712e9fde998bb26
Qray <ilyasch2020@gmail.com>
2025-12-17 18:54:59 +0700

    Restyle account page and fix avatar storage

--- shared/firebase.js
+++ shared/firebase.js
@@ -8,7 +8,7 @@ export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCI1Vp5CSBu0veADOHtuUcFATSaOnXPswc",
   authDomain: "galenite-glnt.firebaseapp.com",
   projectId: "galenite-glnt",
-  storageBucket: "galenite-glnt.firebasestorage.app",
+  storageBucket: "galenite-glnt.appspot.com",
   messagingSenderId: "94081808482",
   appId: "1:94081808482:web:2f5f4842c293c18621f93b",
   measurementId: "G-WT9LDP619Q",
